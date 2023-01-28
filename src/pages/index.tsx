65b8752555ff1fb2e026a56f7fdc1f91d1da3f63
Marco T O Valeriano <marco.valeriano@bancointer.com.br>
2023-01-28 20:25:46 -0300

    capa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,9 +16,28 @@ export default function Home() {
     <main className={styles.main}>
       {openChat ? <Chat handleOpenChat={handleOpenChat} /> : null}
       {!openChat && (
-        <button onClick={handleOpenChat(true)} className={styles.openChat}>
-          Abrir Chat
-        </button>
+        <div className={styles.textsContainer}>
+          <div className={styles.texts}>
+            <h1>
+              <strong>Desafio #04</strong> - Chat
+            </h1>
+            <p>
+              e postar nas redes sociais com a hashtag{' '}
+              <strong>#boraCodar</strong>.
+            </p>
+          </div>
+          <button onClick={handleOpenChat(true)} className={styles.openChat}>
+            Abrir Chat
+          </button>
+          <footer>
+            <p>
+              &copy; 2023{' '}
+              <a href="https://github.com/tuliooov"> Tuliooov #BoraCodar</a>
+              <br></br>
+              Desafio 04 - Run until you fly!
+            </p>
+          </footer>
+        </div>
       )}
     </main>
   )
