3271f4b6f369512a050a7877cebb8b423c0007b6
Marco T O Valeriano <marco.valeriano@bancointer.com.br>
2023-01-28 17:41:24 -0300

    Updates

---
+++ static/chunks/95b64a6e-028d995ab3b09871.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[866],{1649:function(t,n,u){u.d(n,{ghy:function(){return e}});var a=u(8357);function e(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M48 448l416-192L48 64v149.333L346 256 48 298.667z"}}]})(t)}}}]);
